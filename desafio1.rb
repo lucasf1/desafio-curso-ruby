01281456398f16e55410b4cb2ff0cb3176180326
lucasf1 <lucasf1@gmail.com>
2024-11-06 20:36:51 -0300

    commit inicial

---
+++ desafio1.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome:  "
+nome = gets.chomp
+
+print "Digite seu sobrenome:  "
+sobrenome = gets.chomp
+
+print "Digite a sua idade:  "
+idade = gets.chomp.to_i
+
+puts "Olá, #{nome} #{sobrenome}, você possui #{idade} anos."
